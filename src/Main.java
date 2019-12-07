b01269614fd2695ddb4fa66025a1c1057eb77dd9
Roger <rogerpietrzak92@gmail.com>
2019-12-07 15:07:05 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main{
+
     public static void main(String[] args) {
+        List<String> tab = new ArrayList<>();
+        tab.add("a");
+        tab.add("b");
+        tab.add("c");
+        tab.add("d");
+        tab.add("e");
+
+        System.out.println(tab.toString());
+
+        //
+
+        Name("Roger");
+
 
     }
+    public static void Name(String name){
+        System.out.println("My name is: "+ name);
+    }
 }
+
