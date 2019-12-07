8f3f16cf16ad8d9c267d3b52997bde10154ec2b2
Claudiadem <dembowska.klaudia12@gmail.com>
2019-12-07 12:34:58 +0100

    Beng

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,17 @@ final class Main {
         System.out.println("Hello Me");
         System.out.println("Hello Team");
         System.out.println("szczałka");
-        System.out.println(50/3);
-
-
+        System.out.println(50 / 3);
 
 
         for (int i = 0; i < 10; i++) {
             System.out.println("Iteracja: " + i);
         }
 //esddfgfdgdg
+
+        for (int y = 2; y < 15; y++) {
+            System.out.println("Beng" + y);
+        }
 
     }
 }
